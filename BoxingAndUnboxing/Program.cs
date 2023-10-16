2e2f6311c8aebf438eb6ed5f52a18739ec2af9e7
Florin Lazar <florin.lazar.it2014@gmail.com>
2023-10-16 20:54:28 +0300

    boxing, unboxing, literals, operators

---
+++ BoxingAndUnboxing/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace BoxingAndUnboxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // boxing
+            int i = 3;
+            object obj1 = i;
+            Console.WriteLine(i);
+            Console.WriteLine(obj1);
+
+            // unboxing
+            int smallerNumber = 10;
+            object obj2 = smallerNumber;
+            if (obj2 is int)
+            {
+                long j = (int)obj2;
+                Console.WriteLine(obj2);
+                Console.WriteLine(j);
+            }
+
+
+            object obj3 = new Person();
+            if (obj3 is string obj3Content)
+            {
+                Console.WriteLine(obj3Content);
+            }
+            
+
+            // literals
+            long a = 123;
+            long b = -a;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            // operators
+            string text = null;
+            Console.WriteLine("Length of text:" + text?.Length);
+           
+        }
+    }
+
+    public class Person
+    {
+        public string Name;
+    }
+}
